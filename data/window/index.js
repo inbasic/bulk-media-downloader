3efc98ea496f4c93a559d3ccc4f8db2100e8629b
InBasic <inb.cor@gmail.com>
2017-09-03 10:17:22 +0430

    adding header for window directory

--- data/window/index.js
+++ data/window/index.js
@@ -1,3 +1,23 @@
+/*******************************************************************************
+    Bulk Media Downloader - Grab and download media (image and video) sources by monitoring network (like FlashGot)
+
+    Copyright (C) 2014-2017 InBasic
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License as published by
+    the Mozilla Foundation, either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Mozilla Public License for more details.
+    You should have received a copy of the Mozilla Public License
+    along with this program.  If not, see {https://www.mozilla.org/en-US/MPL/}.
+
+    Home: http://add0n.com/media-tools.html
+    GitHub: https://github.com/inbasic/bulk-media-downloader/
+*/
+
 /* globals $ */
 'use strict';
 
